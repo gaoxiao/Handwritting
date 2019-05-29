2556d17214e2f98497dea1b089f94061b7fde9d0
Xiao Gao <gaoxiao1988@gmail.com>
2019-05-28 17:51:55 -0700

    Save and clear.

--- app/src/main/java/ai/ll/handwritting/MainActivity.java
+++ app/src/main/java/ai/ll/handwritting/MainActivity.java
@@ -1,7 +1,16 @@
 package ai.ll.handwritting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void save(View view) {
+        SignaturePad pad = findViewById(R.id.signature_pad);
+        Bitmap bmp = pad.getSignatureBitmap();
+
+        try (FileOutputStream out = openFileOutput("test.png", Context.MODE_PRIVATE)) {
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+            Toast.makeText(getApplicationContext(), "Saved.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clear(View view) {
+        SignaturePad pad = findViewById(R.id.signature_pad);
+        pad.clear();
     }
 }
