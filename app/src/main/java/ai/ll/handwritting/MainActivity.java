b28a019bb8717fc865d72f7fbe7a24a7071c7f54
Xiao Gao <gaoxiao1988@gmail.com>
2019-06-07 15:24:48 -0700

    Use index file.

--- app/src/main/java/ai/ll/handwritting/MainActivity.java
+++ app/src/main/java/ai/ll/handwritting/MainActivity.java
@@ -12,11 +12,13 @@ import android.view.View;
 import android.widget.TextView;
 
 import android.widget.Toast;
+
 import com.github.gcacace.signaturepad.views.SignaturePad;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,64 +28,85 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
     private static final int PIXEL_WIDTH = 28;
     private TextView resText;
-    private String expectNumber="1";
+    private String expectNumber = "1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         resText = (TextView) findViewById(R.id.textView);
-        resText.setText(String.valueOf("draw "+expectNumber));
+        resText.setText(String.valueOf("draw " + expectNumber));
         //loadModel();
     }
-    private String saveToInternalStorage(Bitmap bitmapImage){
+
+    private String saveToInternalStorage(Bitmap bitmapImage) {
+
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
+
         // path to /data/data/yourapp/app_data/imageDir
-        String fileName=expectNumber.replace("/","_");
-        File directory = cw.getDir("ImageOf "+fileName, Context.MODE_PRIVATE);
+        File directory = cw.getDir("saved_data", Context.MODE_PRIVATE);
         // Create imageDir
-        File mypath=new File(directory,fileName+"Num"+ UUID.randomUUID()+".PNG");
+        File mypath = new File(directory, UUID.randomUUID() + ".png");
+        File index = new File(directory, "index.txt");
         FileOutputStream fos = null;
+        OutputStreamWriter myOutWriter = null;
         try {
             fos = new FileOutputStream(mypath);
             // Use the compress method on the BitMap object to write image to the OutputStream
             bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            myOutWriter = new OutputStreamWriter(new FileOutputStream(index, true));
+            myOutWriter.append(String.format("%s,%s\n", mypath.getName(), expectNumber));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
                 fos.close();
+                myOutWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return directory.getAbsolutePath();
     }
+
     public void save(View view) {
         SignaturePad pad = findViewById(R.id.signature_pad);
         Bitmap bmp = pad.getSignatureBitmap();
         saveToInternalStorage(bmp);
+        clear(view);
     }
 
     public void clear(View view) {
         SignaturePad pad = findViewById(R.id.signature_pad);
         Random random = new Random();
-        switch(random.nextInt(3)){
-            case 0:
-                expectNumber=""+random.nextInt(10001);
-                break;
-            case 1:
-                expectNumber=""+random.nextInt(101)+"."+random.nextInt(10)+""+random.nextInt(10);
-                break;
-            case 2:
-                long a=(random.nextInt(100)+1);
-                long b=(random.nextInt(99)+2);
-                long gcm=gcm(a,b);
-                expectNumber=""+(a/gcm)+"/"+(b/gcm);
-                break;
+        int dice = random.nextInt(20);
+        if (dice < 5) {
+            expectNumber = "" + random.nextInt(10);
+        } else if (dice < 7) {
+            expectNumber = "" + random.nextInt(100);
+        } else if (dice < 9) {
+            expectNumber = "" + random.nextInt(1000);
+        } else if (dice < 11) {
+            expectNumber = "" + random.nextInt(1000);
+        } else if (dice < 14) {
+            expectNumber = "" + random.nextInt(10) + "." + random.nextInt(10);
+        } else if (dice < 15) {
+            expectNumber = "" + random.nextInt(100) + "." + random.nextInt(100);
+        } else if (dice < 18) {
+            long a = (random.nextInt(9) + 1);
+            long b = (random.nextInt(9) + 1);
+            long gcm = gcm(a, b);
+            expectNumber = "" + (a / gcm) + "/" + (b / gcm);
+        } else if (dice < 20) {
+            long a = (random.nextInt(99) + 1);
+            long b = (random.nextInt(99) + 1);
+            long gcm = gcm(a, b);
+            expectNumber = "" + (a / gcm) + "/" + (b / gcm);
         }
-        resText.setText(String.valueOf("draw "+expectNumber));
+        resText.setText(expectNumber);
         pad.clear();
     }
+
     public static long gcm(long a, long b) {
         return b == 0 ? a : gcm(b, a % b);
     }
