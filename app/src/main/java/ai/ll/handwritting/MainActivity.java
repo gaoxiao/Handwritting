bba39c82965db8b93d785825ebb99daf293fe23c
tigerchensh <tigersh@Outlook.com>
2019-06-21 13:27:15 -0700

    fix bug for update

--- app/src/main/java/ai/ll/handwritting/MainActivity.java
+++ app/src/main/java/ai/ll/handwritting/MainActivity.java
@@ -152,7 +152,6 @@ public class MainActivity extends AppCompatActivity {
                     HttpResponse response = client.execute(post);
                     HttpEntity httpEntity = response.getEntity();
                     result = EntityUtils.toString(httpEntity);
-                    mypath.delete();
                 } catch (Exception e) {
                     Looper.prepare();
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -163,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Saved.", Toast.LENGTH_SHORT).show();
                 Looper.loop();
                 Log.e("result", result);
+                mypath.delete();
             }
         }).start();
     }
